0d98010f7f938fef8847dfc1bd1b5337e632dfda
Faaiz <fmhs9009@gmail.com>
2025-09-10 20:01:05 +0530

    prod changes 2

--- fe/src/components/CreatedContests.jsx
+++ fe/src/components/CreatedContests.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useAuth0 } from '@auth0/auth0-react';
+import { buildApiUrl } from '../config/api.js';
 
 // Premium Theme Colors matching ContestMode.jsx
 const themeColors = {
